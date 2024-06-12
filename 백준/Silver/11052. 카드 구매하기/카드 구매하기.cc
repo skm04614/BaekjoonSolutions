a33b1794bb40f22995df3c211204c6a0c3ac11fc
skm04614 <77950368+skm04614@users.noreply.github.com>
2024-06-12 19:57:31 +0900

    [Silver I] Title: 카드 구매하기, Time: 0 ms, Memory: 1120 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <cstdio>
+
+unsigned long long DP[1001];
+
+int main(void)
+{
+	int N;
+	scanf("%d", &N);
+
+	for (int i = 1; i <= N; i++)
+	{
+		scanf("%d", &DP[i]);
+		for (int j = 1; j < i; j++)
+		{
+			int tmp = DP[i - j] + DP[j];
+			DP[i] = DP[i] > tmp ? DP[i] : tmp;
+		}
+	}
+	printf("%d", DP[N]);
+
+	return 0;
+}
