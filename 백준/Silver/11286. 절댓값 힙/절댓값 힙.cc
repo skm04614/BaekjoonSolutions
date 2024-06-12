1abfb2cf2f75e96c29f9ee68b67528ff574aca71
skm04614 <77950368+skm04614@users.noreply.github.com>
2024-06-12 19:58:52 +0900

    [Silver I] Title: 절댓값 힙, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -0,0 +1,133 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+
+template <typename T>
+inline void swap(T& x, T& y)
+{
+	T tmp = x;
+	x = y;
+	y = tmp;
+}
+
+template <typename T>
+inline T abs(T x)
+{
+	return x < 0 ? -x : x;
+}
+
+template <typename T>
+bool cmp(T& x, T& y)
+{
+	if (abs(x) == abs(y))
+	{
+		return x < y;
+	}
+
+	return abs(x) < abs(y);
+}
+
+
+struct MinHeap
+{
+	int data[100010];
+	int size;
+
+	MinHeap() : size{ 0 }, data{}
+	{
+
+	}
+
+	void push(int val)
+	{
+		data[++size] = val;
+
+		int idx = size;
+		while (idx > 1)
+		{
+			if (cmp(data[idx], data[idx / 2]))
+			{
+				swap(data[idx], data[idx / 2]);
+				idx /= 2;
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+
+	int pop()
+	{
+		if (size == 0)
+		{
+			return 0;
+		}
+
+		int ret = data[1];
+		swap(data[1], data[size--]);
+		
+		int idx = 1;
+		for (;;)
+		{
+			const int lchidx = 2 * idx;
+			const int rchidx = lchidx + 1;
+
+			if (lchidx > size)
+			{
+				break;
+			}
+
+			int chidx = lchidx;
+			if (size >= rchidx && cmp(data[rchidx], data[lchidx]))
+			{
+				chidx = rchidx;
+			}
+
+			if (cmp(data[chidx], data[idx]))
+			{
+				swap(data[chidx], data[idx]);
+				idx = chidx;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		return ret;
+	}
+
+
+
+};
+
+MinHeap g_min_heap;
+
+
+int main(void)
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+
+	int n, val;
+	std::cin >> n;
+	
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> val;
+
+		if (val == 0)
+		{
+			std::cout << g_min_heap.pop() << '\n';
+		}
+		else
+		{
+			g_min_heap.push(val);
+		}
+	}
+
+	return 0;
+}
