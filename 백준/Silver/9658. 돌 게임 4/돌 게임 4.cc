b1b78bd739a1ba1a8402f52c5da30fa080f1e4f4
skm04614 <77950368+skm04614@users.noreply.github.com>
2024-06-12 19:49:08 +0900

    [Silver II] Title: 돌 게임 4, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.cc
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream> 
+
+int DP[1010];
+
+int max(int x, int y)
+{
+	return x > y ? x : y;
+}
+
+int main(void)
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	int n;
+	std::cin >> n;
+
+	DP[1] = 0;
+	DP[2] = 1;
+	DP[3] = 0;
+	DP[4] = 1;
+
+	for (int i = 5; i <= 1000; i++)
+	{
+		DP[i] = (DP[i - 1] == 0) || (DP[i - 3] == 0) || (DP[i - 4] == 0);
+	}
+
+	std::cout << (DP[n] == 1 ? "SK" : "CY");
+	 
+
+
+	return 0;
+}
