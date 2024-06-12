c9b636fef291d072d542ae982003094d5bf1fddc
skm04614 <77950368+skm04614@users.noreply.github.com>
2024-06-12 19:59:28 +0900

    [Platinum III] Title: 열혈강호 4, Time: 116 ms, Memory: 12724 KB -BaekjoonHub

---
+++ 백준/Platinum/11378. 열혈강호 4/열혈강호 4.cc
@@ -0,0 +1,126 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+struct Adj
+{
+    int v;
+    Adj* next;
+
+    Adj(int v, Adj* next) : v{ v }, next{ next }
+    {
+
+    }
+
+    ~Adj()
+    {
+        delete next;
+        next = nullptr;
+    }
+};
+
+Adj* ADJ[1010];
+int N;
+
+void cleanup()
+{
+    for (int a = 1; a <= N; ++a)
+    {
+        delete ADJ[a];
+        ADJ[a] = nullptr;
+    }
+}
+
+int B[1010];
+bool VISITED[1010];
+int M;
+int K;
+
+void reset()
+{
+    for (int b = 1; b <= M; ++b)
+    {
+        VISITED[b] = false;
+    }
+}
+
+int dfs(const int a)
+{
+    for (Adj* adj = ADJ[a]; adj; adj = adj->next)
+    {
+        const int b = adj->v;
+        if (VISITED[b])
+        {
+            continue;
+        }
+        VISITED[b] = true;
+
+        if (!B[b] || dfs(B[b]))
+        {
+            B[b] = a;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main(void)
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    std::cin >> N >> M >> K;
+    for (int a = 1; a <= N; ++a)
+    {
+        int cnt;
+        std::cin >> cnt;
+        while (cnt--)
+        {
+            int b;
+            std::cin >> b;
+            ADJ[a] = new Adj{ b, ADJ[a] };
+        }
+    }
+
+    int res = 0;
+    for (int a = 1; a <= N; ++a)
+    {
+        reset();
+
+        if (dfs(a))
+        {
+            ++res;
+        }
+
+        if (res == M)
+        {
+            break;
+        }
+    }
+
+    for (int a = 1; a <= N; ++a)
+    {
+        bool flag = true;
+        while (K)
+        {
+            if (!dfs(a))
+            {
+                break;
+            }
+
+            ++res;
+            --K;
+        }
+
+        if (res == M || K == 0)
+        {
+            break;
+        }
+    }
+    std::cout << res;
+
+    cleanup();
+    return 0;
+}
