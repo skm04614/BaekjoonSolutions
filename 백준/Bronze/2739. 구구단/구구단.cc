a5199d326f45739ac2b5dd225cf0ebe4f601e952
skm04614 <77950368+skm04614@users.noreply.github.com>
2024-06-12 19:31:45 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <cstdio>
+
+int main(void)
+{
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i < 10; i++)
+	{
+		printf("%d * %d = %d\n", n, i, n * i);
+	}
+	
+	return 0;
+}
