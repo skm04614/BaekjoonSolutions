c53df675430139440eac33797b853715900f0873
Sukki (Kevin) Moon <77950368+skm04614@users.noreply.github.com>
2024-06-12 20:38:51 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <cstdio>
+
+
+int main(void)
+{
+	printf("         ,r'\"7\n");
+	printf("r`-_   ,'  ,/\n");
+	printf(" \\. \". L_r'\n");
+	printf("   `~\\/\n");
+	printf("      |\n");
+	printf("      |\n");
+
+	return 0;
+}
