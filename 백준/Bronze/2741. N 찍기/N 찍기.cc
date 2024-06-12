0006b5f0af44946cbccbadf52ad058a6a24d1f90
skm04614 <77950368+skm04614@users.noreply.github.com>
2024-06-12 19:31:54 +0900

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <cstdio>
+
+int main(void)
+{
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)
+	{
+		printf("%d\n", i);
+	}
+	
+	return 0;
+}
