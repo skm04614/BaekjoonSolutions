24b1d4a2cd15716aae058c712d1d13506337cd95
Sukki (Kevin) Moon <77950368+skm04614@users.noreply.github.com>
2024-06-12 20:24:41 +0900

    [Silver I] Title: 1, 2, 3 더하기 9, Time: 4 ms, Memory: 9080 KB -BaekjoonHub

---
+++ 백준/Silver/16195. 1， 2， 3 더하기 9/1， 2， 3 더하기 9.cc
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <cstdio>
+
+int T;
+unsigned long long DP[1010][1010];
+
+int main(void)
+{
+	DP[1][1] = 1;
+	DP[2][1] = 1;
+	DP[2][2] = 1;
+	DP[3][1] = 1;
+	DP[3][2] = 2;
+	DP[3][3] = 1;
+
+	for (int n = 4; n <= 1000; n++)
+	{
+		for (int m = 1; m <= n; m++)
+		{
+			DP[n][m] = (DP[n - 1][m - 1] + DP[n - 2][m - 1] + DP[n - 3][m - 1]) % 1000000009;
+		}
+	}
+
+	scanf("%d", &T);
+	while (T--)
+	{
+		int N, M;
+		scanf("%d %d", &N, &M);
+		unsigned long long sol = 0;
+		for (int m = 1; m <= M; m++)
+		{
+			sol = ((sol + DP[N][m]) % 1000000009);
+		}
+		printf("%llu\n", sol);
+	}
+
+	return 0;
+}
