900db0dbdebb347deed28f082ca7d16dad083312
skm04614 <77950368+skm04614@users.noreply.github.com>
2024-06-12 19:56:36 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <cstdio>
+#include <string>
+
+int main(void)
+{
+	int T;
+	scanf("%d", &T);
+
+	for (int t = 1; t <= T; t++)
+	{
+		int a, b;
+		scanf("%d %d", &a, &b);
+		printf("Case #%d: %d\n", t, a + b);
+	}
+	
+	return 0;
+}
