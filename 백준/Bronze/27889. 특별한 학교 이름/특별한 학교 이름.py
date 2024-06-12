77cdf698a803d324cbe428e981600512b7102d0c
Sukki (Kevin) Moon <77950368+skm04614@users.noreply.github.com>
2024-06-12 20:40:12 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,6 @@
+school_dict = {"NLCS": "North London Collegiate School",
+"BHA": "Branksome Hall Asia",
+"KIS": "Korea International School",
+"SJA": "St. Johnsbury Academy"}
+
+print(school_dict[input()])
