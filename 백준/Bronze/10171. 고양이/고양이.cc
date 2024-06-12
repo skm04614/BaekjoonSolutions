3068e61a8fb64335f95f62f2f44a7dd4a79d6f1b
skm04614 <77950368+skm04614@users.noreply.github.com>
2024-06-12 19:50:01 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <cstdio>
+
+int main(void)
+{
+	printf("\\    /\\\n");
+	printf(" )  ( ')\n");
+	printf("(  /  )\n");
+	printf(" \\(__)|\n");
+
+	return 0;
+}
+
+// 
