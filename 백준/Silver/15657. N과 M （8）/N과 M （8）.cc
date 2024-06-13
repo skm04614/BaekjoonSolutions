724508c9f86bd2f94dacaf73f81c715442ecccbf
Sukki (Kevin) Moon <77950368+skm04614@users.noreply.github.com>
2024-06-13 09:56:05 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,76 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+
+int N, M;
+int SOL[10];
+int POOL[10] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+bool VISITED[10];
+
+void _swap(int& x, int& y)
+{
+	int tmp = x;
+	x = y;
+	y = tmp;
+}
+
+void _insertion_sort(int* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		int min_idx = i;
+
+		for (int j = i + 1; j < size; j++)
+		{
+			if (arr[j] < arr[min_idx])
+			{
+				min_idx = j;
+			}
+		}
+
+		_swap(arr[i], arr[min_idx]);
+	}
+}
+
+void sol(int sidx, int depth)
+{
+	if (depth == M)
+	{
+		for (int i = 0; i < depth; i++)
+		{
+			std::cout << SOL[i] << ' ';
+		}
+		std::cout << '\n';
+		return;
+	}
+
+	for (int i = sidx; i < N; i++)
+	{
+		if (VISITED[i])
+		{
+			continue;
+		}
+
+		SOL[depth] = POOL[i];
+		sol(i, depth + 1);
+	}
+}
+
+int main(void)
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	
+	std::cin >> N >> M;
+	for (int i = 0; i < N; i++)
+	{
+		std::cin >> POOL[i];
+	}
+	_insertion_sort(POOL, N);
+
+	sol(0, 0);
+
+	return 0;
+}
