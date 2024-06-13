a3bf25632ab558f9d219ecbc64c2ff666206bcb3
Sukki (Kevin) Moon <77950368+skm04614@users.noreply.github.com>
2024-06-13 09:56:41 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+int ARR[9];
+int RECORD[9];
+int N, M;
+
+void sol(int depth, int sidx)
+{
+	if (depth == M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			std::cout << RECORD[i] << ' ';
+		}
+		std::cout << '\n';
+		return;
+	}
+	
+	int last_chosen_val = 0;
+	for (int i = sidx; i < N; i++)
+	{
+		if (ARR[i] == last_chosen_val)
+		{
+			continue;
+		}
+
+		last_chosen_val = ARR[i];
+		RECORD[depth] = ARR[i];
+		sol(depth + 1, i);
+	}
+}
+
+int main(void)
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+
+	std::cin >> N >> M;
+	for (int i = 0; i < N; i++)
+	{
+		std::cin >> ARR[i];
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		int _i = i;
+		for (int j = i + 1; j < N; j++)
+		{
+			if (ARR[j] < ARR[_i])
+			{
+				_i = j;
+			}
+		}
+
+		int tmp = ARR[_i];
+		ARR[_i] = ARR[i];
+		ARR[i] = tmp;
+	}
+
+	sol(0, 0);
+
+	return 0;
+}
