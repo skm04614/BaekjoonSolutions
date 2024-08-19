e5cceb3906c7cfd54878698d6e7d1f66cbd5bfeb
Sukki (Kevin) Moon <77950368+skm04614@users.noreply.github.com>
2024-08-20 00:33:33 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,16 +1,26 @@
-WITH tmp AS (
-SELECT
-  fish_info.id,
-  fish_type,
-  MAX(length) AS length
-FROM fish_info
-GROUP BY fish_type
+WITH fish AS (
+  SELECT
+    id,
+    fni.fish_type,
+    fish_name,
+    length
+  FROM fish_name_info fni
+  JOIN fish_info fi
+    ON fni.fish_type = fi.fish_type
 )
+
 
 SELECT
   id,
   fish_name,
-  length
-FROM tmp t
-JOIN fish_name_info fni
-  ON t.fish_type = fni.fish_type
+  f.length
+FROM fish f
+JOIN (
+  SELECT
+    fish_type,
+    MAX(length) AS length
+  FROM fish
+  GROUP BY fish_type
+  ) derived
+  ON derived.fish_type = f.fish_type
+    AND derived.length = f.length
